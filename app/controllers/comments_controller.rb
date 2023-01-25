50087bc7ccc207941ea9810376a1969bd879c7a6
Nathaniel Simon <95974868+deLaVerendrye@users.noreply.github.com>
2023-01-25 09:24:00 +0800

    Finsihed Comments CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,25 @@ class CommentsController < ApplicationController
         @post = Post.find(params[:post_id])
         @comment.post = @post
         @comment.user = current_user
-        if @comment.save
-            redirect_to root_path
-        else
+        if !@comment.save
             render :new
         end
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        if !@comment.update(get_params)
+            render :edit
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   resources :likes, only: [ :destroy ]
+  resources :comments, only: [ :destroy, :edit, :update ]
 
   resources :posts do
     resources :likes, only: [ :create ]
-    resources :comments
+    resources :comments, except: [ :edit, :update, :destroy ]
   end
   
   devise_for :users
